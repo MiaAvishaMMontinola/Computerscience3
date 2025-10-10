1734855b55523bfe98b0a268f2eb0f101617515f
MiaAvishaMMontinola <miaavisha.montinola@smc.pshs.edu.ph>
2025-10-10 23:59:43 +0800

    Add files via upload

---
+++ Delivery_Fee.py
@@ -0,0 +1,15 @@
+#Define function for delivery fee
+def calc_delivery_fee(distance, rate):
+    fee = distance*rate
+    total_fee = round(fee,2)
+    return total_fee
+
+#Ask for distance and rate
+distance = float(input("Enter distance in kilometers: "))
+rate = float(input("Enter rate per kilometer (₱): "))
+
+#Calculate delivery fee
+total_fee = calc_delivery_fee(distance, rate)
+
+#Show results
+print(f"Total Delivery Fee: ₱", total_fee)
